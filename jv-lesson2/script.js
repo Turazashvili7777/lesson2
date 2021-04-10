a3cbbcb16de4e35ead5c11275d72686a16769c07
Turazashvili7777 <turazashvili2021@mail.ru>
2021-04-10 14:40:16 +0600

    lesson2

---
+++ jv-lesson2/script.js
@@ -0,0 +1,91 @@
+//1
+// let num = prompt(`Введите число от 1 до 5`)
+
+// let num = `5`
+
+// if (num == 1) {
+//     document.write(`<P>Мизинец</P>`)
+// } else if (num == 2) {
+//     document.write(`<P>Безымянный палец</P>`)
+// } else if (num == 3) {
+//     document.write(`<P>Средний палец</P>`)
+// } else if (num == 4) {
+//     document.write(`<P>Указательный палец</P>`)
+// } else if (num == 5) {
+//     document.write(`<P>Большой палец</P>`)
+// } else {
+//     document.write(`<P>error</P>`)
+// }
+
+
+//2
+// let m1 = prompt(`Введите первое число`)
+// let m2 = prompt(`Введите второе число`)
+
+
+// if (m1 % m2 == 0) {
+//     document.write(`<P>Делится</P>`)
+// } else {
+//     document.write(`<P>не делиться ${m1 % m2}</P>`)
+// }
+
+// let g1 = +prompt(`Введите первое положительное число`)
+// let g2 = +prompt(`Введите второе положительное число`)
+
+// if (g1 < 0 || g2 < 0) {
+//     document.write(`<P>Условие не выполняется</P>`)
+// } else {
+//     document.write(`<P>сумма: ${g1 + g2} </P>`)
+// }
+
+//3
+// var h1 = prompt(`введите число`)
+// var h2 = prompt(`введите число`)
+// var h3 = prompt(`введите число`)
+
+// if (h1 % 5 == 0) {
+//     document.write(`<P>${h1}</P>`)
+// }
+// if (h2 % 5 == 0) {
+//     document.write(`<P>${h2}</P>`)
+// }
+// if (h3 % 5 == 0) {
+//     document.write(`<P>${h3}</P>`)
+
+
+
+//4  (1)Задание не понял ? ? ? ? ? ? ?
+
+
+//4(2)
+
+
+// let yaer = 2025
+//    (1)
+// if (yaer % 4 != 0) {
+//     document.write(`<P>${yaer}:365</P>`)
+// } else if (yaer % 100 == 0) {
+//     if (yaer % 400 == 0) {
+//         document.write(`<P>${yaer}:366</P>`)
+//     } else {
+//         document.write(`<P>${yaer}:365</P>`)
+//     }
+// } else {
+//     document.write(`<P>${yaer}:366</P>`)
+// }
+
+//     (2)
+
+
+// if (yaer % 4 != 0 || (yaer % 100 == 0 && yaer % 400 != 0)) {
+//     document.write(`<P>${yaer}:365</P>`)
+// } else {
+//     document.write(`<P>${yaer}:366</P>`)
+// }
+
+
+
+
+
+
+/////////////////
